7f674ed479e730e6b5dafb76a6416efc7882a5ae
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2021-12-10 01:00:25 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2021, vrajroham'
+author = 'vrarjroham'
 
 release = '0.1'
 version = '0.1.0'
